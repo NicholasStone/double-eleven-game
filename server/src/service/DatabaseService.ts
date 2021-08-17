f3886e3eabe025db738dce5a5c86781d303981b1
Nicholas Stone <fuyuan.shi@garena.cn>
2021-08-17 18:18:15 +0800

    change database to sqlite

--- server/src/service/DatabaseService.ts
+++ server/src/service/DatabaseService.ts
@@ -4,22 +4,22 @@ import Config from '../config'
 const DatabaseConfig = Config.database
 
 const sequelize = new Sequelize({
-  dialect: 'mysql',
-  host: DatabaseConfig.host,
-  port: DatabaseConfig.port,
-  username: DatabaseConfig.username,
-  password: DatabaseConfig.password,
-  database: DatabaseConfig.database,
-  // storage: DatabaseConfig.path,
+  dialect: 'sqlite',
+  // host: DatabaseConfig.host,
+  // port: DatabaseConfig.port,
+  // username: DatabaseConfig.username,
+  // password: DatabaseConfig.password,
+  // database: DatabaseConfig.database,
+  storage: DatabaseConfig.path,
   define: {
     freezeTableName: true,
     paranoid: true, // 采取软删除措施, 如果查询的时候想要查询所有的内容 可以在语句后面加 { paranoid: false }
     timestamps: true,
-    underscored: true, // 驼峰命名转换为下划线
+    underscored: true // 驼峰命名转换为下划线
     // createdAt: 'created_at',
     // updatedAt: 'update_at',
     // deletedAt: 'deleted_at',
-  },
+  }
 })
 
 try {
